2d25b4d448a5350d5d690f371d2a68faf9383e26
chockyland <carlos.choc@nearshoretechnology.com>
2014-05-30 14:55:53 -0500

    addind styles to the windows

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,31 @@
-$('body').on('click', function(e){
-  e.preventDefault();
-  $('#projectPicker').hide();
+CreateTaskView = Backbone.View.extend({
+	events : {
+		'click #project'  : 'openProjectList',
+		'click #close'    : 'closeProjectList',
+		'click a.project' : 'selectProject'
+	},
+ 	initialize: function(){
+    	this.$el = $('#createTask');
+    },
+    openProjectList : function (e) {
+    	e.preventDefault();
+    	this.$('#projectPicker').fadeIn();
+    },
+    closeProjectList : function (e) {
+    	e.preventDefault();
+    	this.$('#projectPicker').fadeOut();
+    },
+    selectProject : function (e) {
+    	e.preventDefault();
+    	var project = $(e.target).text();
+    	if (project.length > 26) {
+    		project = project.substring(0, 23).concat('...');
+    	}
+    	this.$('#project').text(project);
+    	this.$('#projectPicker').fadeOut();
+    }
 });
 
-
-$('#project').on('click', function(e){
-  e.preventDefault();
-  $('#projectPicker').fadeIn();
+$(document).on('ready', function () {
+	new CreateTaskView();
 });
